1033df28831973e104c78437d9ff6948963a636b
Vuxz123 <ethnicthv123@gmail.com>
2022-09-28 09:18:19 +0700

    MVP-VERSION <UPDATE 1>
    (cherry picked from commit 41ea9f036cd862011950a42b523efa98769976f3)

--- src/main/java/com/ethnicthv/bigproject/entity/boom/AbstractBoom.java
+++ src/main/java/com/ethnicthv/bigproject/entity/boom/AbstractBoom.java
@@ -3,7 +3,7 @@ package com.ethnicthv.bigproject.entity.boom;
 import com.ethnicthv.bigproject.client.map.SafeGrid;
 
 public abstract class AbstractBoom {
-    public abstract Runnable getBoomFunc();
+    public abstract Runnable getBoomFunc(int centerX, int centerY);
 
     public abstract SafeGrid.CellUnSafeFunction getCellFunc();
 }
--- src/main/java/com/ethnicthv/bigproject/entity/boom/HBoom.java
+++ src/main/java/com/ethnicthv/bigproject/entity/boom/HBoom.java
@@ -1,13 +1,25 @@
 package com.ethnicthv.bigproject.entity.boom;
 
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.SpawnData;
 import com.ethnicthv.bigproject.client.GameManager;
 import com.ethnicthv.bigproject.client.map.SafeGrid;
+import javafx.geometry.Point2D;
 
 public class HBoom extends AbstractBoom {
     @Override
-    public Runnable getBoomFunc() {
+    public Runnable getBoomFunc(int centerX, int centerY) {
         return () -> {
-
+            int x = centerX;
+            int l = x + 9;
+            for (int i = 0; i < 4; i++) {
+                SpawnData data = new SpawnData(centerX,centerY);
+                data.put("dir", new Point2D(-1,0));
+                FXGL.getGameWorld().spawn("f", data);
+                data = new SpawnData(centerX,centerY);
+                data.put("dir", new Point2D(1,0));
+                FXGL.getGameWorld().spawn("f", data);
+            }
         };
     }
 
