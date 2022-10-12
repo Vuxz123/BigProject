fd59f66ccd1f146e50d74f7d81a32c3591087d7b
user B <21020658@vnu.edu.vn>
2022-10-12 18:24:06 +0700

    save

--- src/main/java/com/ethnicthv/bigproject/client/controller/GameSoundMenuController.java
+++ src/main/java/com/ethnicthv/bigproject/client/controller/GameSoundMenuController.java
@@ -1,6 +1,7 @@
 package com.ethnicthv.bigproject.client.controller;
 
 import java.io.File;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -68,7 +69,11 @@ public class GameSoundMenuController implements Initializable, UIController {
         //System.out.println(FXGL.getAssetLoader().getURL("assets/music/HeySun.mp3").toString());
 
         //directory = new File(FXGL.getAssetLoader().getURL("assets/music/HeySun.mp3").toString()).getParentFile();
-        directory = new File("D:\\Java\\BigProject4\\BigProject\\src\\main\\resources\\assets\\music");
+        try {
+            directory = new File(getClass().getClassLoader().getResource("assets/music").toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
         files = directory.listFiles();
         //System.out.println(files.toString());
         if(files != null) {
